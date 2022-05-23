7d85a43444177f5e60ad131cfc5e86993787ece0
RashidMmdov <tu7lw2f82@code.edu.az>
2022-05-23 11:12:02 +0400

    Add .NET intro

---
+++ First task/First task/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace First_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    return Content("Hello World!");
+        //}
+        //public ViewResult Index()
+        //{
+        //    ViewResult view = new ViewResult
+        //    {
+        //        ViewName = "Index"
+        //    };
+        //    return view;
+        //}
+        //public ViewResult About()
+        //{
+        //    return new ViewResult { ViewName = "About" };
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
